41f8f65fe5559ed45abb9f05d5a0f8330611a2d9
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-07-06 17:19:22 +0200

    aggiunta primo esercizio

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,18 +1,17 @@
-const nameList = ["Giorgia" , "Gianni" ,"Matteo" , "Romano" , "Silvio", "Pierferdinando"]
-const surnameList = ["Giorgini" , "Giannini" , "Matteini" ,"Romanini","Silvini","Ferdinandini"]
-const fakeArray=[]
+const nameList = ["Giorgia", "Gianni", "Matteo", "Romano", "Silvio", "Pierferdinando"]
+const surnameList = ["Giorgini", "Giannini", "Matteini", "Romanini", "Silvini", "Ferdinandini"]
+const fakeArray = []
 
 
 for (let i = 0; i < nameList.length; i++) {
-    const randomName= Math.floor(Math.random() * nameList.length);
-    const randomSurname= Math.floor(Math.random() * surnameList.length);
+    const randomName = Math.floor(Math.random() * nameList.length);
+    const randomSurname = Math.floor(Math.random() * surnameList.length);
 
     const currentName = nameList[randomName];
-    const currentSurname = surnameList[randomSurname]    
+    const currentSurname = surnameList[randomSurname]
 
     const fakeName = `${currentName} ${currentSurname}`
-/*     console.log (fakeName)
- */    fakeArray.push(fakeName)
+    fakeArray.push(fakeName)
 
 }
 
